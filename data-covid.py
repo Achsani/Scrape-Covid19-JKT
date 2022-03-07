25bf79b563542ebdfc395de1c4cf18ba6148d981
farhanachsani <farhanachsani@gmail.com>
2022-03-07 22:08:54 +0700

    change code and update data till 7/3
    change code to include 'Luar DKI Jakarta' and 'Proses Update Data' row

--- data-covid.py
+++ data-covid.py
@@ -24,7 +24,6 @@ res = BeautifulSoup(r.html.raw_html, "html.parser")
 for link in res.find_all('a', href=True):
     data.append(link)
 
-
 # Getting google drive id for newest data
 # Missing data on date 31 Januari  
 # 1 Januari 2021 = 339
@@ -36,7 +35,7 @@ judul = data[7].get_text()
 
 
 # Download excel data using google drive id
-destination = './' + judul + ".xlsx"
+destination = 'C:/Users/Farhan Achsani/Desktop/Kodingan/Scrape-Covid19-JKT/' + judul + ".xlsx"
 gdd.download_file_from_google_drive(file_id=id_file, dest_path=destination)
 judul_excel = judul + ".xlsx"
 
@@ -45,6 +44,7 @@ judul_excel = judul + ".xlsx"
 # 7 Arpil 2020 = 601
 # Newest date on 1
 today = DT.date.today()
+# date = today
 date = today - DT.timedelta(days=1)
 str_date = str(date)
 
@@ -56,19 +56,16 @@ str_date = str(date)
 # # (0 = A, ID Kel) (2 = C, nama_kota) (3 = D, nama_kecamatan) (26 = AA, POSITIF) (27 = AB, Dirawat) (28 = AC, Sembuh) (29 = AD, Meninggal) (30 = AE, Self Isolation)
 cols_kecamatan = [0, 2, 3, 26, 27, 28, 29, 30]
 
-# # Column used for date 7 April 2020
-# # (0 = A, ID Kel) (2 = C, nama_kota) (3 = D, nama_kecamatan) (10 = K, POSITIF) (11 = L, Dirawat) (12 = M, Sembuh) (13 = N, Meninggal) (14 = O, Self Isolation)
-# cols_kecamatan = [0, 2, 3, 10, 11, 12, 13, 14]
-
 # Read downloaded data and exctract column that's needed
-kecamatan = pd.read_excel(judul_excel, header = 0,sheet_name="data_kecamatan", usecols=cols_kecamatan)
-kecamatan.rename(columns={'Meninggal.1': 'Meninggal'}, inplace=True)
+kecamatan = pd.read_excel(destination, header = 0,sheet_name="data_kecamatan", usecols=cols_kecamatan)
+kecamatan.rename(columns={'Meninggal.1': 'Meninggal', 'Self Isolation':'Self_Isolation', 'Kasus Aktif':'Kasus_Aktif'}, inplace=True)
 kecamatan = kecamatan.iloc[1:]
 # kecamatan = kecamatan.iloc[:-1]
-kecamatan['Kasus Aktif'] = kecamatan['Dirawat'] + kecamatan['Self Isolation']
+kecamatan['Kasus_Aktif'] = kecamatan['Dirawat'] + kecamatan['Self_Isolation']
 kecamatan['Tanggal'] = date
-kecamatan = kecamatan[kecamatan["ID_KEC"] != 'LUAR DKI JAKARTA']
-kecamatan = kecamatan[kecamatan["ID_KEC"] != 'PROSES UPDATE DATA'] 
+# kecamatan = kecamatan[kecamatan["ID_KEC"] != 'LUAR DKI JAKARTA']
+# kecamatan = kecamatan[kecamatan["ID_KEC"] != 'PROSES UPDATE DATA'] 
+kecamatan['ID_KEC'] = kecamatan['ID_KEC'].replace(['LUAR DKI JAKARTA', 'PROSES UPDATE DATA'],111111)
 
 # Column used for date 1 Jan 2021 - 18 Jan 2021
 # (0 = A, ID Kel) (2 = C, nama_kota) (3 = D, nama_kecamatan) (4 = E, nama_kelurahan) (26 = AA, POSITIF) (27 = AB, Dirawat) (28 = AC, Sembuh) (29 = AD, Meninggal) (30 = AE, Self Isolation)
@@ -78,31 +75,27 @@ kecamatan = kecamatan[kecamatan["ID_KEC"] != 'PROSES UPDATE DATA']
 # # (0 = A, ID Kel) (2 = C, nama_kota) (3 = D, nama_kecamatan) (4 = E, nama_kelurahan) (27 = AB, POSITIF) (28 = AC, Dirawat) (29 = AD, Sembuh) (30 = AE, Meninggal) (31 = AF, Self Isolation)
 cols_kelurahan = [0, 2, 3, 4, 27, 28, 29, 30, 31]
 
-# Column used for date 7 April 2020
-# # (0 = A, ID Kel) (3 = D, nama_kota) (4 = E, nama_kecamatan) (5 = F, nama_kelurahan) (12 = M, POSITIF) (13 = N, Dirawat) (14 = O, Sembuh) (15 = P, Meninggal) (16 = Q, Self Isolation)
-# cols_kelurahan = [0, 3, 4, 5, 12, 13, 14, 15, 16]
-
 # Read downloaded data and extract column that's needed
-kelurahan = pd.read_excel(judul_excel, header = 0, sheet_name="data", usecols=cols_kelurahan)
+kelurahan = pd.read_excel(destination, header = 0, sheet_name="data", usecols=cols_kelurahan)
 kelurahan.rename(columns={'Meninggal.1': 'Meninggal'}, inplace=True)
 kelurahan = kelurahan.iloc[1:]
 kelurahan['Kasus Aktif'] = kelurahan['Dirawat'] + kelurahan['Self Isolation']
 kelurahan['Tanggal'] = date
-kelurahan = kelurahan[kelurahan["ID_KEL"] != 'LUAR DKI JAKARTA']
-kelurahan = kelurahan[kelurahan["ID_KEL"] != 'PROSES UPDATE DATA'] 
+#kelurahan = kelurahan[kelurahan["ID_KEL"] != 'LUAR DKI JAKARTA']
+#kelurahan = kelurahan[kelurahan["ID_KEL"] != 'PROSES UPDATE DATA'] 
+kelurahan['ID_KEL'] = kelurahan['ID_KEL'].replace(['LUAR DKI JAKARTA', 'PROSES UPDATE DATA'],111111)
 
 # Print downloaded data and date
 print(judul_excel)
 print(date)
 
 # Location of old data that we want to append
-destination_kecamatan = './Covid19-JKT-2021-Kecamatan.csv'
-destination_kelurahan = './Covid19-JKT-2021-Kelurahan.csv'
+destination_kecamatan = 'C:/Users/Farhan Achsani/Desktop/Kodingan/Scrape-Covid19-JKT/Covid19-JKT-2021-Kecamatan.csv'
+destination_kelurahan = 'C:/Users/Farhan Achsani/Desktop/Kodingan/Scrape-Covid19-JKT/Covid19-JKT-2021-Kelurahan.csv'
 
 #Read past data
 past_kelurahan = pd.read_csv(destination_kelurahan)
 past_kecamatan = pd.read_csv(destination_kecamatan)
-
 
 # Append past data with new data
 past_kelurahan = past_kelurahan.append(kelurahan)
@@ -117,7 +110,3 @@ os.remove(destination_kelurahan)
 # For iteration other than the first time
 past_kecamatan.to_csv(destination_kecamatan,index=False)
 past_kelurahan.to_csv(destination_kelurahan,index=False)
-
-# # For first iteration to csv
-# kecamatan.to_csv(destination_kecamatan,index=False)
-# kelurahan.to_csv(destination_kelurahan,index=False)
